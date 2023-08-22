fa33c875dd222c80bef0aba9ab4dbf1ede8de5cf
onkemetse <collenonkemetse@gmail.com>
2023-08-21 22:33:01 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -32,11 +32,11 @@ int _printf(const char *format, ...)
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			//flags = get_flags(format, &i);
-			//width = get_width(format, &i, list);
-			//precision = get_precision(format, &i, list);
-			//size = get_size(format, &i);
-			//++i;
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
 			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
 			if (printed == -1)
